f4b202043b99dd7f785aa1226f5417ee008feacc
Michael J. Simons <michael@simons.ac>
2017-05-10 13:13:16 +0200

    Reformat

--- src/main/java/de/springbootbuch/logging_with_log4j/Application.java
+++ src/main/java/de/springbootbuch/logging_with_log4j/Application.java
@@ -2,6 +2,7 @@ package de.springbootbuch.logging_with_log4j;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,23 +10,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Part of springbootbuch.de.
+ *
+ * @author Michael J. Simons
+ * @author @rotnroll666
+ */
 @SpringBootApplication
 public class Application {
 
-    @RestController
-    @RequestMapping("/hello")
-    public static class HelloWorldController {
+	@RestController
+	@RequestMapping("/hello")
+	public static class HelloWorldController {
 
-        private static final Log LOG = LogFactory.getLog(HelloWorldController.class);
+		private static final Log LOG
+			= LogFactory.getLog(HelloWorldController.class);
 
-        @GetMapping
-        public String helloWorld(@RequestParam final String name) {
-            LOG.debug("Calling helloWorld");
-            return "Hello, " + name + "\n";
-        }
-    }
+		@GetMapping
+		public String helloWorld(@RequestParam String name) {
+			LOG.debug("Calling helloWorld");
+			return "Hello, " + name + "\n";
+		}
+	}
 
-    public static void main(String... args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
